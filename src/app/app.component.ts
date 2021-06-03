594d4629e23191f84ea6196700701e864a06e0b3
Joemar Bagalanon <jbagalanon@gmail.com>
2021-06-03 08:59:47 +0800

    added faker addons & add text binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { lorem } from 'faker';
+
+lorem.sentence();
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'typingidiot';
+  randomText = lorem.sentence();
+  solved = false;
+
+  onInput(value: string) {
+    if (value === this.randomText) {
+      this.solved = true;
+    }
+  }
 }
